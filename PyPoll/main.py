459422fb1ea425c2d4e58f99cae15b321f14bea7
Victor <victorbakr@gmail.com>
2021-03-30 21:23:28 -0500

    pyPoll Script version 0.1

---
+++ PyPoll/main.py
@@ -0,0 +1,14 @@
+# Objectives
+import csv
+open('Resources\election_data',"r") # change this to importing data filee using csv
+# The total number of votes cast
+votes = rows(dataset ) - 1
+
+# A complete list of candidates who received votes
+with dataset(column3):
+    #identify unique values in this column, turn this column into a set?
+# the percentage of votes each candidate won
+for x in()
+# the total number of votes each candidate won
+
+# The winner of the election based on popular vote

f043ef3974a36ebb29152214f1675d8e76c2be7a
Victor <victorbakr@gmail.com>
2021-03-30 20:41:47 -0500

    version 0.1

---
+++ PyBank/main.py
@@ -0,0 +1,16 @@
+data = open(Resources\'Budget_data.csv')
+# Objectives
+
+# Total number of months in the data set
+#   Use the len function to count the number of rows in the data set
+months = len(datasetrows) or rows(data) 
+#The net total amount of pofit losses over the entier period 
+#Summation of all rows column 2
+netprofit = 0
+ For x in range(1,months+1):
+     netprofit = netprofit + x,column2
+     
+     
+     
+     
+     

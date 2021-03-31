77749234017d60a8243c920fff852fb69c4a5ce0
Victor <victorbakr@gmail.com>
2021-03-30 20:52:51 -0500

    version 0.1

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,8 +9,19 @@ months = len(datasetrows) or rows(data)
 netprofit = 0
  For x in range(1,months+1):
      netprofit = netprofit + x,column2
-     
-     
-     
-     
-     
+
+# The average of the changes in profit/losses over the entier period
+average= netprofit/months
+
+#The greatest increase in profits over the entire period
+max = 0
+For x in range(1,months+1):
+    If x > max:
+        max = x
+ min = 0       
+# the greatest decrease in losses over the entire period
+    For x in range(1,months+1):
+        If x < min:
+            min = x
+# print to terminal
+#export results 

3878c4ab59e3cfaf475dd8bc483e8d1885424652
Victor <victorbakr@gmail.com>
2021-04-03 00:21:15 -0500

    version 0.3

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,36 +1,30 @@
-# dependencies
+# Dependencies
 import os
 import csv
 
-# locate the data set
+# Locate the data set
 filepath = os.path.join("Resources","budget_data.csv")
-numrows = 0
+months = 0
 netprofit = 0
 date = []
 monthlyprofits = []
-# read in the data set
+# Get operating parameters from the data set
 with open(filepath) as temp:
     budgetdata = csv.reader(temp,delimiter=",")
-    print(budgetdata)
-    
-    # iterate through each row    
+    headers = next(budgetdata)     
     for rows in budgetdata:
-        numrows = numrows + 1 # row counting
-        # read data into a list
+        months += 1 
         date.append(rows[0])
         monthlyprofits.append(rows[1])
-    months = numrows - 1
-    print(numrows)
-    print(monthlyprofits[months])    
+   
 # Net profit function        
-for x in range(1,numrows):
+for x in range(months):
     netprofit = netprofit + int(monthlyprofits[x])
-print(netprofit)
-    
-#The greatest increase in profits over the entire period
+
+# The greatest increase in profits over the entire period
 maxvalue = 0
 maxdate = ""
-for x in range(1,numrows):
+for x in range(months):
     if int(monthlyprofits[x]) > maxvalue:
         maxvalue = int(monthlyprofits[x])
         maxdate = str(date[x])
@@ -38,40 +32,34 @@ for x in range(1,numrows):
 minvalue = 0
 mindate = ""
 # the greatest decrease in losses over the entire period
-for x in range(1,numrows):
+for x in range(months):
     if int(monthlyprofits[x]) < minvalue:
             minvalue = int(monthlyprofits[x])
             mindate = str(date[x])
+
 average= (round(netprofit/months,2))
-
-print(months) #testing counter
-print(monthlyprofits)
-print(netprofit)  
-print(minvalue)
-print(maxvalue)
-print(average)      
-
 # The average of the changes in profit/losses over the entier period
 
 
 # print to terminal
-print("Profit Analysis")
-print("----------------------")
-print(f"Time Period: {months} months")
-print(f"Net Profit: ${netprofit}")
-print(f"Average Profit: ${average}")
-print(f"Greatest gain: {maxdate} ${maxvalue}")
-print(f"Greatest loss: {mindate} ${minvalue}")
+print(f'''Profit Analysis
+----------------------
+Time Period: {months} months
+Net Profit: ${netprofit}
+Average Profit: ${average}
+Greatest gain: {maxdate} ${maxvalue}
+Greatest loss: {mindate} ${minvalue}''')
+
 #export results 
 import sys
 defaultout = sys.stdout
-with open('Analysis\pyBank_output.txt','w') as text:
+with open('Analysis\PyBank_output.txt','w') as text:
     sys.stdout = text
-    print("Profit Analysis")
-    print("----------------------")
-    print(f"Time Period: {months} months")
-    print(f"Net Profit: ${netprofit}")
-    print(f"Average Profit: ${average}")
-    print(f"Greatest gain: {maxdate} ${maxvalue}")
-    print(f"Greatest loss: {mindate} ${minvalue}")
+    print(f'''Profit Analysis
+    ----------------------
+    Time Period: {months} months
+    Net Profit: ${netprofit}
+    Average Profit: ${average}
+    Greatest gain: {maxdate} ${maxvalue}
+    Greatest loss: {mindate} ${minvalue}''')
 sys.stdout = defaultout
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,16 @@
-# dependencies
+# Dependencies
 import os
 import csv
 
-file_path = os.path.join("Resources","election_data.csv")
+file_path = os.path.join("Resources","election_data.csv") # Data location
 
 # Get operating parameters from the data set
 totalvotes = 0
 uniquenames = set()
 
-with open(file_path,"r") as temp:
-    electiondata = csv.reader(temp, delimiter=",")     
-    headers=next(electiondata) # store headers
+with open(file_path,"r") as temp: 
+    electiondata = csv.reader(temp, delimiter=",") # read in the CSV file  
+    headers = next(electiondata) # store headers
     for rows in electiondata:
         totalvotes += 1
         uniquenames.add(rows[2])        
@@ -24,7 +24,7 @@ with open(file_path,"r") as temp:
     for rows in electiondata:
         for names in candidates:
             if rows[2] == names:
-                votes[candidates.index(names)] +=1
+                votes[candidates.index(names)] += 1
 
 # convert votes to percentage
 percentvotes = [round((x/totalvotes)*100,2) for x in votes]                       
@@ -44,13 +44,17 @@ print(f'''--------------------------------
 Winner: {candidates[winner]}''')
 
 #Output to text file
-print(f'''Fictional State Election Results
---------------------------------
-Total Votes: {totalvotes}
---------------------------------''')
-for person in candidates:
-    x = candidates.index(person)
-    print(f'{candidates[x]}: {percentvotes[x]}% ({votes[x]})')
-print(f'''--------------------------------
-Winner: {candidates[winner]}''')
-
+import sys
+defaultout = sys.stdout
+with open('Analysis\PyPoll_output.txt','w') as text:
+    sys.stdout = text
+    print(f'''Fictional State Election Results
+    --------------------------------
+    Total Votes: {totalvotes}
+    --------------------------------''')
+    for person in candidates:
+        x = candidates.index(person)
+        print(f'{candidates[x]}: {percentvotes[x]}% ({votes[x]})')
+        print(f'''--------------------------------
+        Winner: {candidates[winner]}''')
+sys.stdout = defaultout

e5e08a20522dbb8467c67178863d7aa782f101cd
Victor <victorbakr@gmail.com>
2021-04-02 22:11:26 -0500

    version 0.3

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,29 +17,21 @@ candidates = list(uniquenames)
 votes = [0] * len(candidates)
 print(f'votes: {votes}')
 
-
 # count votes
 with open(file_path,"r") as temp:
-    electiondata = csv.reader(temp, delimiter=",")   
+    electiondata = csv.reader(temp, delimiter=",")
+    
     for rows in electiondata:
         for names in candidates:
             if rows[2] == names:
                 votes[candidates.index(names)] +=1
-            
-               
+
+percentvotes = [round((x/totalvotes)*100,2) for x in votes]                       
 print(totalvotes)
 print(candidates)
 print(votes)
-    
+print(percentvotes) 
   
 #%%
-
-# the total number of votes each candidate won
-whovotes[len(canditates)] = 0
-with candidates(column3)
-    for x in range(2,dataset rows +1):
-       whovotes(candidates(x)) = whovotes(candidates(x)) +1
-# the percentage of votes each candidate won
-percentvotes = (whovotes/votes) * 100 #round this to 1 decimal
 
 # The winner of the election based on popular vote

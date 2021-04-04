68bee83ed8735bebf46089150cc231c67123b9d0
Victor <victorbakr@gmail.com>
2021-04-04 00:29:52 -0500

    version 2.0

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,81 +1,47 @@
-# Function PyPoll, returns results from election data
 def PyPoll(input_path,output_path):
-    
-    #Dependencies
     import csv
     
-    # Get operating parameters from the data set
-    totalvotes = 0
-    uniquenames = set()
-    with open(input_path,"r") as temp: 
+    #Analyze the data
+    with open(datalocation,"r") as temp: 
         electiondata = csv.reader(temp, delimiter=",") # read in the CSV file  
         headers = next(electiondata) # store headers
+        print(f'Headers: {headers}')
+        candidatelist = {}
         for rows in electiondata:
-            totalvotes += 1
-            uniquenames.add(rows[2])
- 
-    # Count the votes
-    candidates = list(uniquenames)
-    votes = [0] * len(candidates)
-    with open(input_path,"r") as temp:
-        electiondata = csv.reader(temp, delimiter=",")
-        for rows in electiondata:
-            for names in candidates:
-                if rows[2] == names:
-                    votes[candidates.index(names)] += 1
-    
-    # convert votes to percentage
-    percentvotes = [round((x/totalvotes)*100,2) for x in votes]                       
-    
-    # determine the position of the winner
-    winner = votes.index(max(votes))  
-    
-    # Output results to terminal
-    print(f'''Fictional State Election Results
+            if rows[2] in candidatelist.keys():
+                candidatelist[rows[2]]+=1
+            else:
+                candidatelist[rows[2]] = 1
+                   
+        totalvotes = sum(candidatelist.values())
+        winner = [candidate for candidate,votes in candidatelist.items() if votes == max(candidatelist.values())]
+        
+    # Format the output of election results   
+    def resultrecorder():      
+        print(f'''Election Results
 --------------------------------
 Total Votes: {totalvotes}
 --------------------------------''')
-    for person in candidates:
-        x = candidates.index(person)
-        print(f'{candidates[x]}: {percentvotes[x]}% ({votes[x]})')
-    print(f'''--------------------------------
-Winner: {candidates[winner]}''')
+        for candidate in candidatelist:
+            votes = candidatelist.get(candidate)
+            percentvotes = round((votes/totalvotes)*100,2)
+            print(f'{candidate}: {percentvotes}% {votes}')
+        print(f'''--------------------------------
+Winner: {str(winner).replace("[","").replace("'","").replace("]","")}''')
+    # Output results to the terminal
+    resultrecorder()
     
-    #Output results to text file
+    # Output results to specified text file
     import sys
     defaultout = sys.stdout
     with open(output_path,'w') as text:
         sys.stdout = text
-        print(f'''Fictional State Election Results
---------------------------------
-Total Votes: {totalvotes}
---------------------------------''')
-        for person in candidates:
-            x = candidates.index(person)
-            print(f'{candidates[x]}: {percentvotes[x]}% ({votes[x]})')
-        print(f'''--------------------------------
-Winner: {candidates[winner]}''')
-    sys.stdout = defaultout
+        resultrecorder()
+    sys.stdout =defaultout
 
-# Call PyPoll
+#Call PyPoll
 import os
 
 datalocation = os.path.join("Resources","election_data.csv")
 saveto = os.path.join("Analysis","PyPoll_Output.txt")
-
-# PyPoll(datalocation,saveto)
-import csv
-with open(datalocation,"r") as temp: 
-    electiondata = csv.reader(temp, delimiter=",") # read in the CSV file  
-    headers = next(electiondata) # store headers
-    candidatelist = {}
-    for rows in electiondata:
-        if rows[2] in candidatelist.keys():
-            candidatelist[rows[2]]+=1
-        else:
-            candidatelist[rows[2]] = 1
-    print(candidatelist)      
-    # for rows in electiondata:
-        
-        
-
+PyPoll(datalocation,saveto)       
